3cf2d0f7a7dbb3a957ccfcf61978b4cbf25e5d42
Himaja-Battina <124758137+Himaja-Battina@users.noreply.github.com>
2023-02-21 10:37:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def welcome():
-    return "hii welcome to ibm isdl labs!"
+    return "hello welcome to ibm isdl labs!"
 # if __name__=="__main__":
 #     app.run(debug=True)
 #added new comment
+
 """from flask import Flask
 
 app = Flask(__name__)
